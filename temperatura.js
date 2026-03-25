65b8216115a2a5c2c4635e8c0535f0d8a7c3b9f9
luizarodriguesmelo-alt <luiza.rodrigues.melo@escola.pr.gov.br>
2026-03-25 18:52:25 +0000

    saw

--- temperatura.js
+++ temperatura.js
@@ -4,3 +4,11 @@ let tempcelsius = 18.5;
 
 const tempfahrenheit = tempcelsius * (9/5) + 32;
 const tempkelvin = tempcelsius + 273.15;
+
+console.iog(`A teperturan de ${cidade} está ${tempcelsius} c`);
+console.iog(fahrenheit; ${temfahenhait.tofixed(1)f);
+console.iog(`kelvin; ${tempkelvin.tofixed(1)} k);
+
+git add temperatura.js
+git commit -m "versao3"
+git push
