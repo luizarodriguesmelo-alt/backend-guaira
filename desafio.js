e51800cc4f488468eecbd626e50120e423dcace6
luizarodriguesmelo-alt <luiza.rodrigues.melo@escola.pr.gov.br>
2026-05-11 19:16:12 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,12 @@
+let idade = 15; 
+let nome = "luiza";
+let serie = "1 A"
+let nascimento = 2008;
+let cpf = "161.124.312.86";
+
+console.log("idade", idade);
+console.log("nome",nome);
+console.log("serie".serie);
+console.log("nascimento",nascimento);
+console.log("cpf",cpf);
+console.log(`Nome: ${nome}, Idade: ${idade}, Série: ${serie}, Ano de nascimento: ${nascimento}, CPF: ${cpf}. você concluiu o cadastro`);
